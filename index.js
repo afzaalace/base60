e917777e8bef712aac5145663ba6f1911e56c0de
Afzaal Ameer <afzaal@afzaalace.com>
2016-05-09 19:23:04 +0200

    Init

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = (function(Base60) {
+	var DEFAULT_CHARACTER_SET = "0123456789abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ";
+	var s, val, base60Chars;
+	Base60.encode = function(integer) {
+		s = '';
+		while (integer > 0) {
+			s = Base60.characterSet[integer % Base60.indexLength] + s;
+			integer = Math.floor(integer / Base60.indexLength);
+		}
+		return s !== '' ? s : '0';
+	};
+
+	Base60.decode = function(base60String) {
+		val = 0;
+		base60Chars = base60String.split("").reverse();
+		base60Chars.forEach(function(character, index) {
+			val += Base60.characterSet.indexOf(character) * Math.pow(Base60.indexLength, index);
+		});
+		return val;
+	};
+
+	Base60.setCharacterSet = function(chars) {
+		var arrayOfChars = chars.split(""),
+			uniqueCharacters = [];
+
+		arrayOfChars.forEach(function(char) {
+			if (!~uniqueCharacters.indexOf(char)) {
+				uniqueCharacters.push(char);
+			} else {
+				throw Error("You must use unique characters.");
+			}
+		});
+		Base60.indexLength = arrayOfChars.length;
+		Base60.characterSet = arrayOfChars;
+	};
+
+	Base60.setCharacterSet(DEFAULT_CHARACTER_SET);
+	return Base60;
+}({}));
---
+++ test/index.js
@@ -0,0 +1,34 @@
+/*jshint mocha: true*/
+var should = require('should'),
+	path = require('path'),
+	Base60 = require(path.join(__dirname, '../index.js'));
+
+describe('Base60', function() {
+	it("should return 0", function(done) {
+		var res = Base60.encode(0);
+		res.should.equal("0");
+		done();
+	});
+	it("should return 4ehm", function(done) {
+		var res = Base60.encode(915442);
+		res.should.equal("4ehm");
+		done();
+	});
+	it("should return 19936", function(done) {
+		var res = Base60.decode("5xg");
+		res.should.equal(19936);
+		done();
+	});
+	it("should return rmnO - base62 ", function(done) {
+		Base60.setCharacterSet("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		var res = Base60.encode(6520900);
+		res.should.equal("rmnO");
+		done();
+	});
+	it("should return error ", function(done) {
+		should.throws(function() {
+			Base60.setCharacterSet("000123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		}, /You must use unique characters\./, "Did not throw message.");
+		done();
+	});
+});
